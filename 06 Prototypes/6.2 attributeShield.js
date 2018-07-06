6aba1b3a8fc1ab7babd25f4ec7bde86b3af1df29
Noah <huiali@hotmail.com>
2018-07-06 13:50:18 +0800

    Update

--- 06 Prototypes/6.2 attributeShield.js
+++ 06 Prototypes/6.2 attributeShield.js
@@ -4,7 +4,7 @@
 // 2. 如果在 [[Prototype]] 链上层存在 foo ，但是它被标记为只读（ writable:false ），那么
 // 无法修改已有属性或者在 myObject 上创建屏蔽属性。如果运行在严格模式下，代码会
 // 抛出一个错误。否则，这条赋值语句会被忽略。总之，不会发生屏蔽。
-// 3. 如果在 [[Prototype]] 链上层存在 foo 并且它是一个 setter（参见第 3 章），那就一定会
+// 3. 如果在 [[Prototype]] 链上层存在 foo 并且它是一个 setter，那就一定会
 // 调用这个 setter。 foo 不会被添加到（或者说屏蔽于） myObject ，也不会重新定义 foo 这
 // 个 setter。
 

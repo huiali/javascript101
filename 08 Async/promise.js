63540b63c5c3902a783cee89e2335fd264d14fbc
Noah <huiali@hotmail.com>
2018-08-20 15:40:19 +0800

    add promise

---
+++ 08 Async/promise.js
@@ -0,0 +1,16 @@
+var p3 = new Promise(function (resolve, reject) {
+    resolve("B");
+});
+var p1 = new Promise(function (resolve, reject) {
+    resolve(p3);
+});
+p2 = new Promise(function (resolve, reject) {
+    resolve("A");
+});
+p1.then(function (v) {
+    console.log(v);
+});
+
+p2.then(function (v) {
+    console.log(v);
+});

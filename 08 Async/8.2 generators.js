b6d8c30806ce2f3d4bf280cccf06db88bf7bfda3
Noah <huiali@hotmail.com>
2018-08-30 10:41:43 +0800

    Generator'ing Values

--- 08 Async/8.2 generators.js
+++ 08 Async/8.2 generators.js
@@ -44,13 +44,13 @@ console.log(res.value); // "Hello"
 res = it.next(3); // 向等待的yield传入3
 console.log(res.value); // 6
 
-///////////////////////////////////////////////////////////////
+///////////////// 多个迭代器 ///////////////////////////
 
 function* foo() {
     var x = yield 2;  //value 2
     z++;
     var y = yield (x * z); //value x *  yield
-    console.log(x, y, z);  
+    console.log(x, y, z);
 }
 var z = 1;
 var it1 = foo();
@@ -65,4 +65,4 @@ val2 = it2.next(val1 * 5).value; // 600 <-- x:200, z:3
 it1.next(val2 / 2); // y:300
 // 20 300 3
 it2.next(val1 / 4); // y:10
-    // 200 10 3
+// 200 10 3
